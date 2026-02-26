272caa72775f49e346597b6fae4f8342bfcd0889
Saurav7985 <sauravraikwar26@gamil.com>
2026-02-26 17:44:27 +0530

    backend and frontend

--- src/app.js
+++ src/app.js
@@ -1,15 +1,14 @@
  const express = require('express')
- const authRouter = require('./routes/auth.routes')
- const cookieParser = require('cookie-parser')
+ const cors = require('cors')
  const quaryRouter = require('./routes/quary.routes')
 
  
  const app = express();
 
  app.use(express.json());
- app.use(cookieParser())
- app.use('/api/auth',authRouter)
  app.use('/api/quary',quaryRouter)
+//  app.use(cors())
+ app.use(express.static('public'))
 
 
 
